ad6128adf52b58b92589a280157e310e6b15e48c
Harald Notini <harald.notini@aventyret.com>
2020-12-03 00:33:45 +0100

    Cleaner without offset

--- day2/day2_2.php
+++ day2/day2_2.php
@@ -2,8 +2,8 @@
 $re = '/^([\d]*)-([\d]*)\s([a-z]):\s([a-z]*)$/';
 $i=0;
 foreach (file('day2data.txt') as $str) {
-    preg_match($re, $str, $matches, PREG_OFFSET_CAPTURE, 0);
-    if ($matches[4][0][$matches[1][0]-1] === $matches[3][0] xor $matches[4][0][$matches[2][0]-1] === $matches[3][0]) {
+    preg_match($re, $str, $matches);
+    if ($matches[4][$matches[1]-1] === $matches[3] xor $matches[4][$matches[2]-1] === $matches[3]) {
         $i++;
     }
 }
